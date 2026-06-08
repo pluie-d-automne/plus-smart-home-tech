1c270f4074ca7b43940cd067cab360ff2dba6bf2
Test <test@example.com>
2026-06-08 23:25:09 +0300

    Add config server

--- telemetry/aggregator/src/main/java/aggregator/kafka_utils/KafkaClient.java
+++ telemetry/aggregator/src/main/java/aggregator/kafka_utils/KafkaClient.java
@@ -4,10 +4,14 @@ import org.apache.avro.specific.SpecificRecordBase;
 import org.apache.kafka.clients.consumer.Consumer;
 import org.apache.kafka.clients.producer.Producer;
 
+import java.util.Properties;
+
 public interface KafkaClient {
     Producer<String, SpecificRecordBase> getProducer();
 
     Consumer<String, SpecificRecordBase> getConsumer();
 
     void stop();
+
+    Properties getProperties();
 }
